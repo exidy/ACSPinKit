d455e0ae7ed7bdb46499b360ebc6b364e1c23255
Orlando Schäfer <orlando.schaefer@arconsis.com>
2015-08-21 16:54:09 +0200

    More documentation for pin controller.

--- Pod/Classes/ACSPinController.h
+++ Pod/Classes/ACSPinController.h
@@ -31,9 +31,24 @@
 
 
 /**
- This is the main interface class you use for accessing all the features of ACSPinKit
+ This is the main interface class you use for accessing all the features of ACSPinKit.
+ 
+ The pin controller provides functionality for 3 use cases: creation, validation and change.
  */
 @interface ACSPinController : NSObject
+
+/**
+ Initializes and returns the pin controller object you can use to make further actions.
+ 
+ @param pinServiceName A service name that is used for storing the pin in the keychain.
+ @param pinUserName A username that is used for storing the pin in the keychain.
+ @param accessGroup The access group used for keychain access. If this parameter is nil the standard group is used.
+ 
+ @return An initialized pin controller object or nil if something went wrong
+ */
+- (instancetype)initWithPinServiceName:(NSString *)pinServiceName pinUserName:(NSString *)pinUserName accessGroup:(NSString *)accessGroup delegate:(id <ACSPinControllerDelegate>)delegate;
+
+/// @name Customization / Configuration
 
 /**
  Customization object for setting different things like colors, titles, messages and images
@@ -51,16 +66,7 @@
  */
 @property (nonatomic, copy) BOOL (^validationBlock)(NSString *pin);
 
-/**
- Initializes and returns the pin controller object you can use to make further actions.
- 
- @param pinServiceName A service name that is used for storing the pin in the keychain.
- @param pinUserName A username that is used for storing the pin in the keychain.
- @param accessGroup The access group used for keychain access. If this parameter is nil the standard group is used.
- 
- @return An initialized pin controller object or nil if something went wrong
- */
-- (instancetype)initWithPinServiceName:(NSString *)pinServiceName pinUserName:(NSString *)pinUserName accessGroup:(NSString *)accessGroup delegate:(id <ACSPinControllerDelegate>)delegate;
+/// @name Touch ID
 
 /**
  Returns if Touch ID is available or not (device does not support it, Touch ID is not enrolled etc.)
@@ -68,48 +74,73 @@
  
  @param error An error pointer for getting additional information in case that no touch id is available
  
- 
  @return YES if Touch ID is available, NO if not.
  */
 - (BOOL)touchIDAvailable:(NSError **)error;
 
+/// @name Presentation
+
 /**
- The controller used for verifying a pin the user enters. You have to present and dismiss the controller manually
+ The controller used for verifying a pin the user enters. You have to present and dismiss the controller manually.
+ 
+ @return The verification controller.
  */
 - (UIViewController *)verifyControllerForCustomPresentation;
 
 /**
  Full screen controller used for verifying a pin the user enters. You have to present and dismiss the controller manually
- In addition you can provide a boolean value that indicates if Touch ID should be used or not.
+ In addition you can provide a boolean value that indicates if Touch ID should be used or not (if YES please ensure you have stored a
+ pin in the keychain via the storePin: message or you've created a pin with the creation controller and did not provide a validation block).
+ 
+ @param touchID A boolean value that indicates if Touch ID should be used.
+ 
+ @return The fullscreen verification controller.
  */
 - (UIViewController *)verifyControllerFullscreenForCustomPresentationUsingTouchID:(BOOL)touchID;
 
 
 /**
  Presents the verification controller from a provided view controller
+ 
+ @param viewController The view controller used for presenting the pin controller.
  */
 - (void)presentVerifyControllerFromViewController:(UIViewController *)viewController;
 /**
  Presents the change pin controller from a provided view controller
+ 
+ @param viewController The view controller used for presenting the pin controller.
  */
 - (void)presentChangeControllerFromViewController:(UIViewController *)viewController;
 /**
  Presents the pin creation controller from a provided view controller
+ 
+ @param viewController The view controller used for presenting the pin controller.
  */
 - (void)presentCreateControllerFromViewController:(UIViewController *)viewController;
 
 
+/// @name Keychain PIN access
+
+
 /**
- Get the current stored pin. The pin will only be stored in the keychain, if you do not provide a validation block.
- If you want to store the pin manually you have to use the storePin: method.
+ Get the current stored pin. The pin will only be stored in the keychain, if you do not provide a validation block and use the
+ pin creation controller. Or you use the storePin: method directly for manually saving your pin.
+ 
+ @return The pin stored in the keychain or nil if there is no pin present.
  */
 - (NSString *)storedPin;
 /**
  Stores a given pin to the keychain.
+ 
+ @param pin The pin you want to store in the keychain.
+ 
+ @return A boolean value that indicates whether the pin was stored successfully.
  */
 - (BOOL)storePin:(NSString *)pin;
 /**
  Removes the stored pin from the keychain.
+ 
+ @return A boolean value that indicates whether the pin was removed successfully.
  */
 - (BOOL)resetPIN;
 
